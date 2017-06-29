3d190cb57c025acb4ee457b003a916354dab207b
yangbiao01 <yangbiao01@beyondsoft.com>
2017-06-29 17:36:20 +0800

    log debugging ok

--- src/3rd_party/apr-1.5.0/threadproc/unix/thread.c
+++ src/3rd_party/apr-1.5.0/threadproc/unix/thread.c
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #include "apr.h"
 #include "apr_portable.h"
 #include "apr_arch_threadproc.h"
